658be99e9d6f14c9cffdae14540d3ec06c89b9cd
idriana <mad_fishmonger@mail.ru>
2021-03-29 21:55:45 +0300

    much much harder

--- app/api/main.py
+++ app/api/main.py
@@ -1,9 +1,11 @@
 from aiohttp.web import run_app, Application
 from app.api import host, port, AppConfig
 from app.api.routes import *
-
+import sys
+from pathlib import Path
 
 def make_app():
+    sys.path.append(Path(__file__).parents[2])
     app = Application(client_max_size=AppConfig.MAX_REQUEST_SIZE)
     app.router.add_view('/couriers', CouriersView)
     app.router.add_view('/couriers/{courier_id:\d+}', CourierView)
